e5489547a6f81b46c3211361c0b22babd0b9b940
Just RAG <justrag@gmail.com>
2025-07-25 15:49:55 +0200

    fix initBoard function

--- board.ts
+++ board.ts
@@ -30,96 +30,53 @@ export const makeEmptyBoard = (): Board =>
  * Cols:    01234567
  */
 export function initBoard(): Board {
-  return [
-    // Row 7: 33321...
-    [
-      { color: "blue", size: 3, orientation: "UP" },
-      { color: "blue", size: 3, orientation: "UP" },
-      { color: "blue", size: 3, orientation: "UP" },
-      { color: "blue", size: 2, orientation: "UP" },
-      { color: "blue", size: 1, orientation: "UP" },
-      {},
-      {},
-      {},
-    ],
-    // Row 6: 3221....
-    [
-      { color: "blue", size: 3, orientation: "UP" },
-      { color: "blue", size: 2, orientation: "UP" },
-      { color: "blue", size: 2, orientation: "UP" },
-      { color: "blue", size: 1, orientation: "UP" },
-      {},
-      {},
-      {},
-      {},
-    ],
-    // Row 5: 321.....
-    [
-      { color: "blue", size: 3, orientation: "UP" },
-      { color: "blue", size: 2, orientation: "UP" },
-      { color: "blue", size: 1, orientation: "UP" },
-      {},
-      {},
-      {},
-      {},
-      {},
-    ],
-    // Row 4: 21.....1
-    [
-      { color: "blue", size: 2, orientation: "UP" },
-      { color: "blue", size: 1, orientation: "UP" },
-      {},
-      {},
-      {},
-      {},
-      {},
-      { color: "red", size: 1, orientation: "UP" },
-    ],
-    // Row 3: 1.....12
-    [
-      { color: "blue", size: 1, orientation: "UP" },
-      {},
-      {},
-      {},
-      {},
-      {},
-      { color: "red", size: 1, orientation: "UP" },
-      { color: "red", size: 2, orientation: "UP" },
-    ],
-    // Row 2: .....123
-    [
-      {},
-      {},
-      {},
-      {},
-      {},
-      { color: "red", size: 1, orientation: "UP" },
-      { color: "red", size: 2, orientation: "UP" },
-      { color: "red", size: 3, orientation: "UP" },
-    ],
-    // Row 1: ....1223
-    [
-      {},
-      {},
-      {},
-      {},
-      { color: "red", size: 1, orientation: "UP" },
-      { color: "red", size: 2, orientation: "UP" },
-      { color: "red", size: 2, orientation: "UP" },
-      { color: "red", size: 3, orientation: "UP" },
-    ],
-    // Row 0: ...12333
-    [
-      {},
-      {},
-      {},
-      { color: "red", size: 1, orientation: "UP" },
-      { color: "red", size: 2, orientation: "UP" },
-      { color: "red", size: 3, orientation: "UP" },
-      { color: "red", size: 3, orientation: "UP" },
-      { color: "red", size: 3, orientation: "UP" },
-    ],
-  ].toReversed() as Board;
+  const a = makeEmptyBoard();
+
+  const blues: Array<[Coords, Size]> = [
+    // [col,row], size
+    [[0, 7], 3],
+    [[1, 7], 3],
+    [[2, 7], 3],
+    [[3, 7], 2],
+    [[4, 7], 1],
+    [[0, 6], 3],
+    [[1, 6], 2],
+    [[2, 6], 2],
+    [[3, 6], 1],
+    [[0, 5], 3],
+    [[1, 5], 2],
+    [[2, 5], 1],
+    [[0, 4], 2],
+    [[1, 4], 1],
+    [[0, 3], 1],
+  ];
+
+  blues.forEach(([coords, size]) =>
+    placePyramid(a, coords, "blue", size, "UP")
+  );
+
+  const reds: Array<[Coords, Size]> = [
+    // [col,row], size
+    [[7, 4], 1],
+    [[6, 3], 1],
+    [[7, 3], 2],
+    [[5, 2], 1],
+    [[6, 2], 2],
+    [[7, 2], 3],
+    [[4, 1], 1],
+    [[5, 1], 2],
+    [[6, 1], 2],
+    [[7, 1], 3],
+    [[3, 0], 1],
+    [[4, 0], 2],
+    [[5, 0], 3],
+    [[6, 0], 3],
+    [[7, 0], 3],
+  ];
+
+  reds.forEach(([coords, size]) => placePyramid(a, coords, "red", size, "UP"));
+
+  return a;
 }
 
 /**
